52ff1c3133a96e7b6c0722f920440380b0bf45fd
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-11-27 15:41:24 +0530

    added gui to our project using tkinter !!! basic skeleton done connection to backend is left

--- gui.py
+++ gui.py
@@ -1,12 +1,20 @@
-from tkinter import *
-from tkinter import messagebox
 
-top = Tk()
-
-C = Canvas(top, bg="blue", height=250, width=300)
-filename = PhotoImage(file="C:\Users\hp\PycharmProjects\TKinter-Basics")
-background_label = Label(top, image=filename)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-C.pack()
-top.mainloop()
+def fun():
+    tkinter.END
+def fu():
+    global toBesearched
+    window=tkinter.Tk()
+    window.geometry("600x400")
+    window.title("new window")
+    tkinter.Label(window,text="hey"+toBesearched.get() ,fg="red").grid(row=10,columnspan=2)
+    tkinter.Button(window,text="button2",fg="red",command=(fun)).grid(row=11,columnspan=2)
+import tkinter
+window=tkinter.Tk()
+# setting the windows size
+window.geometry("600x400")
+window.title("GUI")
+tkinter.Label(window,text="movie liked",fg="blue",bg="pink").grid(row=0,column=0)
+toBesearched=tkinter.Entry(window,fg="black",bg="pink")
+toBesearched.grid(row=0,column=1)
+tkinter.Button(window,text="Login",fg="pink",command=(fu)).grid(row=3,columnspan=2)
+window.mainloop()
